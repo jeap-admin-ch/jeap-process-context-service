8f2eaeddd21dbefcebc6cba7c72b0f2d554ac157
Matthias Fritschi <matthias.fritschi@bit.admin.ch>
2026-03-05 17:11:31 +0100

    JEAP-6540 Add minimum release version validation for PCS v17

---
+++ jeap-process-context-scs/src/main/java/ch/admin/bit/jeap/processcontext/release/PcsReleaseProperties.java
@@ -0,0 +1,31 @@
+package ch.admin.bit.jeap.processcontext.release;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "jeap.processcontext.release")
+public class PcsReleaseProperties {
+
+    private int minVersion = 0;
+
+    @PostConstruct
+    public void validate() {
+        if (minVersion < 17) {
+            throw new MinReleaseNotSetException();
+        }
+    }
+
+    public static class MinReleaseNotSetException extends RuntimeException {
+        public MinReleaseNotSetException() {
+            super("""
+                    IMPORTANT: PCS v17 is a major release with breaking changes. To avoid data loss, you MUST follow the
+                    upgrade guide for version 17.  To ensure that you are aware of this, the minimum release version
+                    property jeap.processcontext.release.min-version must be set to at least 17""");
+        }
+    }
+
+}
