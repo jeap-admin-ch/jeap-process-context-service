f9b13cf56e5a5e1dd766b08c56e6d8d1c3bce2fe
Matthias Fritschi <matthias.fritschi@bit.admin.ch>
2026-03-10 12:21:22 +0100

    JEAP-5249 Disable spring data repository metrics
    Avoids clash with custom metrics

--- jeap-process-context-scs/src/main/java/ch/admin/bit/jeap/processcontext/Application.java
+++ jeap-process-context-scs/src/main/java/ch/admin/bit/jeap/processcontext/Application.java
@@ -1,10 +1,12 @@
 package ch.admin.bit.jeap.processcontext;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.data.RepositoryMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+// Exclude: Disable Spring Data repository metrics to avoid conflicting with @Timed annotations on repository interfaces
+@SpringBootApplication(exclude = RepositoryMetricsAutoConfiguration.class)
 @EnableAsync
 public class Application {
 
