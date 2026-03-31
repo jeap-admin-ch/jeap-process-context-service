46b79f34401dd6f88e2f7a6fb86205f26696b278
Matthias Fritschi <matthias.fritschi@bit.admin.ch>
2026-03-31 15:02:02 +0200

    JEAP-6923 Stabilize UI tests

--- jeap-process-context-ui/cypress.config.ts
+++ jeap-process-context-ui/cypress.config.ts
@@ -1,28 +1,44 @@
-import { defineConfig } from 'cypress';
-import { devServer } from '@cypress/vite-dev-server';
+import {defineConfig} from 'cypress';
+import {devServer} from '@cypress/vite-dev-server';
 import angular from '@analogjs/vite-plugin-angular';
-import { resolve } from 'path';
+
 export default defineConfig({
-  hosts: {
-    localhost: '127.0.0.1',
-  },
-  component: {
-    devServer: function (devServerConfig: Parameters<typeof devServer>[0]) {
-      return devServer({
-        ...devServerConfig,
-        viteConfig: {
-          plugins: [
-            angular({ tsconfig: './tsconfig.json' }),
-          ],
-          resolve: {
-            mainFields: ['es2020', 'es2015', 'browser', 'module', 'main'],
-            alias: {
-              src: resolve(__dirname, 'src'),
-            },
-          },
-        },
-      });
-    },
-    specPattern: '**/*.cy.ts',
-  },
+	hosts: {
+		localhost: '127.0.0.1'
+	},
+	component: {
+		devServer: function (devServerConfig: Parameters<typeof devServer>[0]) {
+			return devServer({
+				...devServerConfig,
+				viteConfig: {
+					plugins: [angular({tsconfig: './tsconfig.json'})],
+					optimizeDeps: {
+						include: [
+							'@angular/common',
+							'@angular/material/button',
+							'@angular/material/card',
+							'@angular/material/expansion',
+							'@angular/material/form-field',
+							'@angular/material/icon',
+							'@angular/material/input',
+							'@angular/material/list',
+							'@angular/material/paginator',
+							'@angular/material/radio',
+							'@angular/material/slide-toggle',
+							'@angular/material/sort',
+							'@angular/material/table',
+							'@angular/material/tooltip'
+						]
+					},
+					resolve: {
+						mainFields: ['es2020', 'es2015', 'browser', 'module', 'main'],
+						alias: {
+							src: resolve(__dirname, 'src')
+						}
+					}
+				}
+			});
+		},
+		specPattern: '**/*.cy.ts'
+	}
 });
