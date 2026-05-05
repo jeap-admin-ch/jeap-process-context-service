b65aff1ed47acec58f99279a53049a361182516c
Fabien Cerf <fabien.cerf@bit.admin.ch>
2026-05-05 14:22:24 +0200

    JEAP-6883: Clean code after upgrading to jeap-spring-boot-parent 34.4 (spring boot 4)

--- jeap-process-context-repository-jpa/src/main/java/ch/admin/bit/jeap/processcontext/adapter/jpa/ProcessContextRepositoryFacadeImpl.java
+++ jeap-process-context-repository-jpa/src/main/java/ch/admin/bit/jeap/processcontext/adapter/jpa/ProcessContextRepositoryFacadeImpl.java
@@ -7,7 +7,6 @@ import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
